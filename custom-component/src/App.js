dd48b4a9d3b6147cf9c9db040dad2576d0128cf5
wjdrbdyd <jky9010@gmail.com>
2022-01-29 11:44:40 +0900

    ClickToEdit unfinished

--- custom-component/src/App.js
+++ custom-component/src/App.js
@@ -1,18 +1,23 @@
 import React from "react";
 import "./App.css";
+import ClickToEdit from "./component/ClickToEdit";
 import Modal from "./component/Modal";
 import Tab from "./component/Tab";
 
 function App() {
   return (
     <div className="App">
-      <div className="container">
+      {/* <div className="container">
         <h3 className="title">Tab</h3>
         <Tab />
       </div>
       <div className="container">
         <h3 className="title">Modal</h3>
         <Modal />
+      </div> */}
+      <div className="container">
+        <h3 className="title">ClickToEdit</h3>
+        <ClickToEdit />
       </div>
     </div>
   );
--- custom-component/src/component/ClickToEdit.js
+++ custom-component/src/component/ClickToEdit.js
@@ -1,7 +1,89 @@
-import React from "react";
+import { getDefaultNormalizer } from "@testing-library/react";
+import React, { useState } from "react";
+import styles from "../css/ClickToEdit.module.css";
 
 const ClickToEdit = () => {
-  return <div></div>;
+  const [person, setPerson] = useState({ name: "김코딩", age: "33" });
+  const [editMode, setEditMode] = useState({ name: false, age: false });
+
+  const [editPerson, setEditPerson] = useState({
+    name: person.name,
+    age: person.age,
+  });
+
+  const changePerson = (e) => {
+    const { name, value } = e.target;
+    console.log("1");
+    setPerson({
+      ...person,
+      [name]: value,
+    });
+  };
+
+  const onEdit = (name) => {
+    console.log("2");
+    setEditMode({ ...editMode, [name]: true });
+  };
+
+  const onBlur = (e) => {
+    console.log("3");
+    const { name, value } = e.target;
+    setEditPerson({ ...person, [name]: value });
+    setEditMode({ ...editMode, [name]: false });
+  };
+
+  // const onEdit = (e) => {
+  //   const { type } = e;
+  //   const { name, value } = e.target;
+
+  //   let status = type === "click" ? true : false;
+  //   setEditPerson({
+  //     ...editPerson,
+  //     [name]: value,
+  //   });
+  //   console.log(status);
+  //   setEditMode({
+  //     ...editMode,
+  //     [name]: status,
+  //   });
+  // };
+  console.log(editMode);
+  return (
+    <div className={styles.edit_wrap}>
+      <div className={styles.view_input_box}>
+        <label>이름</label>
+        {editMode.name ? (
+          <input
+            onBlur={onBlur}
+            name="name"
+            value={person.name}
+            type="text"
+            onChange={changePerson}
+          />
+        ) : (
+          <p onClick={() => onEdit("name")}>{person.name}</p>
+        )}
+      </div>
+      <div className={styles.view_input_box}>
+        <label>나이</label>
+        {editMode.age ? (
+          <input
+            onBlur={onBlur}
+            name="age"
+            value={person.age}
+            type="number"
+            onChange={changePerson}
+          />
+        ) : (
+          <p onClick={() => onEdit("age")}>{person.age}</p>
+        )}
+      </div>
+
+      <p>
+        이름 {editPerson.name} 나이 {editPerson.age}
+      </p>
+    </div>
+  );
 };
 
 export default ClickToEdit;
