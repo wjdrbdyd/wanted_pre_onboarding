bbc390306bdc5766a566511c77372416d3ad3e38
wjdrbdyd <jky9010@gmail.com>
2022-01-30 00:25:19 +0900

    AutoComplete finished

--- custom-component/src/App.js
+++ custom-component/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import AutoComplete from "./component/AutoComplete";
 import ClickToEdit from "./component/ClickToEdit";
 import Modal from "./component/Modal";
 import Tab from "./component/Tab";
@@ -9,25 +10,29 @@ import Toggle from "./component/Toggle";
 function App() {
   return (
     <div className="App">
-      {/* <div className="container">
+      <div className="container">
         <h3 className="title">Tab</h3>
         <Tab />
       </div>
       <div className="container">
         <h3 className="title">Modal</h3>
         <Modal />
-      </div> */}
-      {/* <div className="container">
+      </div>
+      <div className="container">
         <h3 className="title">ClickToEdit</h3>
         <ClickToEdit />
-      </div> */}
-      {/* <div className="container">
+      </div>
+      <div className="container">
         <h3 className="title">Tag</h3>
         <Tag />
-      </div> */}
+      </div>
       <div className="container">
         <h3 className="title">Toggle</h3>
         <Toggle />
+      </div>
+      <div className="container ">
+        <h3 className="title">AutoComplete</h3>
+        <AutoComplete />
       </div>
     </div>
   );
--- custom-component/src/component/AutoComplete.js
+++ custom-component/src/component/AutoComplete.js
@@ -1,7 +1,70 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import styles from "../css/AutoComplete.module.css";
+const data = [
+  {
+    id: 0,
+    name: "antique",
+  },
+  {
+    id: 1,
+    name: "vintage",
+  },
+  {
+    id: 2,
+    name: "중고A급",
+  },
+  {
+    id: 3,
+    name: "rustic",
+  },
+  {
+    id: 4,
+    name: "refurbished",
+  },
+];
 const AutoComplete = () => {
-  return <div></div>;
+  const originList = data;
+  const [searchList, setSearchList] = useState([...originList]);
+  const [searchVal, setSearchVal] = useState("");
+
+  const onChange = (e) => {
+    const copyList = originList.filter((item) => {
+      return item.name.toUpperCase().indexOf(e.target.value.toUpperCase()) > -1;
+    });
+
+    setSearchVal(e.target.value);
+    setSearchList(copyList);
+  };
+
+  const onFocus = (e) => {
+    const copyList = originList.filter((item) => {
+      return item.name.toUpperCase().indexOf(e.target.value.toUpperCase()) > -1;
+    });
+    setSearchList(copyList);
+  };
+
+  const onClick = (e) => setSearchVal(originList[e.target.id].name);
+  const onLeave = (e) => setSearchVal("");
+
+  return (
+    <div className={styles.auto_wrap} onMouseLeave={onLeave}>
+      <input
+        onChange={onChange}
+        onFocus={onFocus}
+        value={searchVal}
+        className={searchVal !== "" && searchList.length > 0 && styles.focus_on}
+      />
+      {searchVal !== "" && searchList.length > 0 && (
+        <ul className={styles.auto_list}>
+          {searchList.map((item) => (
+            <li key={item.id} onClick={onClick} id={item.id}>
+              {item.name}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default AutoComplete;
